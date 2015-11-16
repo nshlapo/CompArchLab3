e5788d4439a35d9edcc76c849392ddcac8909c9b
Sarah Walters <sarah.walters@students.olin.edu>
2015-11-15 21:03:44 -0500

    Changing all instances of li to addi

--- asmtest/euler1.asm
+++ asmtest/euler1.asm
@@ -1,15 +1,15 @@
 # Sum the multiples of 3 and 5 between 0 and 100, inclusive.
 # Expected output: $s0 is 0x972
-li $a0, 100
+addi $a0, $zero, 100
 jal fizzbuzz
 add $s0, $v0, $zero
 
-li $v0, 10
+addi $v0, $zero, 10
 syscall
 
 # Sum the multiples of 3 and 5 between 0 and $a0, inclusive.
 fizzbuzz:
-li $t0, 0
+addi $t0, $zero, 0
 
 fb_loop:
 bne $a0, $zero, fb_continue
@@ -17,7 +17,7 @@ add $v0, $zero, $t0
 jr $ra
 
 fb_continue:
-li $t7, 12
+addi $t7, $zero, 12
 # ...push $ra, $a0
 sub $sp, $sp, $t7
 sw $a0, 8($sp)
@@ -25,17 +25,17 @@ sw $ra, 4($sp)
 sw $t0, 0($sp)
 
 # ...check for divisibility by 3 and 5
-li $a1, 3
+addi $a1, $zero, 3
 jal divBy
 add $t2, $v0, $zero # 1 iff $a0 % 3 == 0
 lw $a0, 8($sp)
-li $a1, 5
+addi $a1, $zero, 5
 jal divBy
 add $t3, $v0, $zero # 1 iff $a0 % 5 == 0
 
 add $t4, $t2, $t3 # 0 iff $a0 not divisible by 3 or 5
 bne $t4, $zero, fb_else
-li $t5, 0
+addi $t5, $zero, 0
 j fb_end
 
 fb_else: 
@@ -47,11 +47,11 @@ fb_end:
 lw $a0, 8($sp)
 lw $ra, 4($sp)
 lw $t0, 0($sp)
-li $t7, 12
+addi $t7, $zero, 12
 add $sp, $sp, $t7
 
 add $t0, $t0, $t5
-li $t7, 1
+addi $t7, $zero, 1
 sub $a0, $a0, $t7
 j fb_loop
 
@@ -62,7 +62,7 @@ sub $t1, $zero, $a1
 
 db_begin:
 bne $a0, $zero, db_continue
-li $v0, 1 # $a0 is 0 -> divisible by $a1
+addi $v0, $zero, 1 # $a0 is 0 -> divisible by $a1
 jr $ra
 
 db_continue:
@@ -72,5 +72,5 @@ add $a0, $a0, $t1
 j db_begin
 
 db_end:
-li $v0, 0
+addi $v0, $zero, 0
 jr $ra
