604398265250a4231d9e6f98b3688e522857c280
nshlapo <nur.shlapobersky@students.olin.edu>
2015-11-16 12:21:41 -0500

    Instruction port added to memory

--- verilog/memory.v
+++ verilog/memory.v
@@ -1,16 +1,19 @@
 module memory(
   input clk,
   input regWE,
-  input[9:0] Addr,
+  input[9:0] DataAddr,
+  input[9:0] InstrAddr,
   input[31:0] DataIn,
-  output[31:0] DataOut
+  output[31:0] DataOut,
+  output[31:0] InstrOut
 );
 
   reg [31:0] mem[1023:0];
   always @(posedge clk)
     if (regWE)
-      mem[Addr] <= DataIn;
+      mem[DataAddr] <= DataIn;
   initial $readmemh("data/average.dat", mem);
 
-  assign DataOut = mem[Addr];
+  assign DataOut = mem[DataAddr];
+  assign InstrOut = mem[InstrAddr]
 endmodule
